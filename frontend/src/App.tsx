d5101204f0c1a4eb999fbb9c9b5c10bec09ef15b
Bamidele Oluwatobi <teebams49@gmail.com>
2022-05-04 12:38:15 +0100

    Frontend  init

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold text-center my-6">
+        BTC Payment Processor
+      </h1>
+    </div>
+  );
+}
+
+export default App;
