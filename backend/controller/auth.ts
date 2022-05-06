fee647cbf573c377f1033a64efb04bba0e1037e0
Bamidele Oluwatobi <teebams49@gmail.com>
2022-05-06 12:55:32 +0100

    feat: Sign-in API

--- backend/controller/auth.ts
+++ backend/controller/auth.ts
@@ -2,7 +2,48 @@ import { ResponseHandler } from "../utils";
 const models = require("../models");
 import bcrypt from "bcrypt";
 const joi = require("joi");
-export const SignIn = () => {};
+import dotenv from "dotenv";
+import jwt from "jsonwebtoken";
+
+dotenv.config();
+
+export const SignIn = async (email: string, password: string) => {
+  const schema = joi.object({
+    email: joi.string().email().required(),
+    password: joi.string().required(),
+  });
+
+  const validation = schema.validate({
+    email,
+    password,
+  });
+
+  if (validation.error) {
+    console.log("Happy");
+    return ResponseHandler(422, validation.error.details[0].message);
+  }
+  const SECRET: any = process.env.JWT_SECRET_TOKEN;
+  try {
+    const user = await models.User.findOne({ where: { email } });
+    if (!user) {
+      return ResponseHandler(404, "Invalid Email or Password");
+    }
+
+    const validatePassword = await bcrypt.compare(password, user.password);
+    if (!validatePassword) {
+      return ResponseHandler(404, "Invalid Email or Password");
+    }
+
+    let token = jwt.sign(
+      { id: user.id, email: user.email, role: user.email },
+      SECRET
+    );
+    return ResponseHandler(200, "Login Successfull", { token });
+  } catch (error) {
+    console.log(error);
+    return ResponseHandler(500, "An Error Occured");
+  }
+};
 
 export const SignUp = async (email: string, password: string) => {
   const schema = joi.object({
@@ -28,7 +69,7 @@ export const SignUp = async (email: string, password: string) => {
       password: bcrypt.hashSync(password, bcrypt.genSaltSync(10)),
       role: "user",
     });
-    return ResponseHandler(200, "Account Created Successfully");
+    return ResponseHandler(201, "Account Created Successfully");
   } catch (error) {
     return ResponseHandler(500, "An Error Occured");
   }
--- backend/routes/auth.ts
+++ backend/routes/auth.ts
@@ -4,7 +4,8 @@ import { SignUp, SignIn } from "../controller/auth";
 const route: Router = express.Router();
 
 route.post("/sign-in", async (req: Request, res: Response) => {
-  const response = SignIn();
+  const response = await SignIn(req.body.email, req.body.password);
+  res.status(response.status).json(response.body);
 });
 
 route.post("/sign-up", async (req: Request, res: Response) => {
