2745f611a8b6d4a2a288e82d2a5a72ffe7f169c3
Collin Rukundo <support@matchstick.ug>
2022-05-07 13:55:08 +0300

    Updated dashboard UI

--- frontend/src/pages/Dashboard.js
+++ frontend/src/pages/Dashboard.js
@@ -1,13 +1,50 @@
 import SideNav from "../elements/SideNav"
+import StatCard from "../components/StatCard"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import {
+    faBriefcase,
+    faBoxArchive,
+    faMoneyBillTransfer
+} from "@fortawesome/free-solid-svg-icons"
 
 function Dashboard() {
     return (
         <div className="flex">
             <SideNav />
             <div className="flex-1 min-h-screen bg-gray-200">
-                <div class="w-full max-w-screen-xl py-6 px-6">
-                    <div class="mb-12">
-                        <h2 class="text-3xl font-bold mb-4">Overview</h2>
+                <div className="w-full max-w-screen-xl py-6 px-6">
+                    <div className="mb-12">
+                        <h2 className="text-3xl font-bold mb-4">Overview</h2>
+                        <div className="flex flex-wrap justify-between">
+                            <StatCard
+                                title="Total Products"
+                                stat={24}
+                                link="/"
+                                icon={<FontAwesomeIcon icon={faBoxArchive} />}
+                                statSize="text-3xl"
+                                className="mb-6 xl:mb-0"
+                            />
+                            <StatCard
+                                title="Total Sats Collected"
+                                stat={"328,743"}
+                                link="/"
+                                icon={<FontAwesomeIcon icon={faMoneyBillTransfer} />}
+                                statSize="text-3xl"
+                                className="mb-6 xl:mb-0"
+                            />
+                            <StatCard
+                                title="Total Transactions"
+                                stat={2}
+                                link="/"
+                                icon={<FontAwesomeIcon icon={faBriefcase} />}
+                                statSize="text-3xl"
+                                className="mb-6 xl:mb-0"
+                            />
+                        </div>
+                    </div>
+
+                    <div className="mb-12">
+                        <h2 className="text-3xl font-bold mb-4">Recent Transactions</h2>
 
                     </div>
                 </div>
