9879691d3aef4acbcd653e8c55828422202f4b0e
Collin Rukundo <support@matchstick.ug>
2022-05-11 14:40:51 +0300

    Added nav guards

--- frontend/src/store/auth-context.js
+++ frontend/src/store/auth-context.js
@@ -9,16 +9,19 @@ const AuthContext = React.createContext({
 })
 
 export const AuthContextProvider = (props) => {
-    const [token, setToken] = useState(null)
+    const initialToken = localStorage.getItem('token');
+    const [token, setToken] = useState(initialToken);
 
     const userIsLoggedIn = !!token; // if token is empty string will return false, if is a string, return true
 
     const loginHandler = () => {
         setToken(token);
+        localStorage.setItem('token', token);
     };
 
     const logoutHandler = () => {
         setToken(null);
+        localStorage.removeItem('token');
     };
 
     const contextValue = {
