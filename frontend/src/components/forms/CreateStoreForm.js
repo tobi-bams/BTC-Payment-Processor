090e29e6b7ea05a40a6ef0f89757ae054463f9f2
Bamidele Oluwatobi <teebams49@gmail.com>
2022-05-23 16:30:13 +0100

    update: getting updated user data after create store

--- frontend/src/components/forms/CreateStoreForm.js
+++ frontend/src/components/forms/CreateStoreForm.js
@@ -1,65 +1,96 @@
-import React, { useState } from 'react';
-import { useHistory } from 'react-router-dom';
-import Button from '../ui/Button';
-import FormGroup from '../ui/FormGroup';
+import React, { useState, useContext } from "react";
+import { useHistory } from "react-router-dom";
+import Button from "../ui/Button";
+import FormGroup from "../ui/FormGroup";
+import axios from "axios";
+import AuthContext from "../../context/auth-context";
+const Swal = require("sweetalert2");
 
 const CreateStoreForm = (props) => {
+  const nameInputRef = React.createRef();
+  const authCtx = useContext(AuthContext);
 
-    const nameInputRef = React.createRef();
+  const [isLoading, setIsLoading] = useState(false);
 
-    const [isLoading, setIsLoading] = useState(false);
+  const history = useHistory();
+  const userToken = localStorage.getItem("token");
 
-    const history = useHistory();
-    const userToken = localStorage.getItem('token');
+  async function submitHandler(event) {
+    event.preventDefault();
 
-    function submitHandler(event) {
-        event.preventDefault();
+    const enteredName = nameInputRef.current.value;
 
-        const enteredName = nameInputRef.current.value;
-
-        setIsLoading(true);
-        fetch('http://localhost:5000/store/create',
-            {
-                method: 'POST',
-                body: JSON.stringify({
-                    name: enteredName
-                }),
-                headers: {
-                    'Content-Type': 'application/json',
-                    'Authorization': `Bearer ${userToken}`
-                }
-            }).then((res) => {
-                setIsLoading(false);
-                if (res.ok) {
-                    return res.json();
-                } else {
-                    return res.json().then(data => {
-                        let errorMsg = 'Something went wrong. Store was not created!';
-                        throw new Error(errorMsg);
-                    });
-                }
-            }).then(data => {
-                localStorage.setItem('storeData', JSON.stringify(data));
-                setIsLoading(false);
-                history.replace("/dashboard/getting-started");
-            })
-            .catch(err => {
-                alert(err.message);
-            });
-    }
-    return (
-        <>
-            <form className="w-full mx-auto text-center" onSubmit={submitHandler}>
-                <FormGroup>
-                    <input className="w-full border border-gray-300 rounded-sm px-4 py-3 outline-none transition-colors duration-150 ease-in-out focus:border-blue-400" type="text" required id="name" ref={nameInputRef} placeholder="Name of your store" />
-                </FormGroup>
-                <FormGroup>
-                    {!isLoading && <Button text="Create my store" submit full />}
-                    {isLoading && <div className="w-full px-6 py-3 rounded-sm border text-gray-800 bg-gray-200 border-gray-300" role="alert">Creating store...</div>}
-                </FormGroup>
-            </form>
-        </>
-    )
-}
+    setIsLoading(true);
+    fetch("http://localhost:5000/store/create", {
+      method: "POST",
+      body: JSON.stringify({
+        name: enteredName,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${userToken}`,
+      },
+    })
+      .then((res) => {
+        setIsLoading(false);
+        if (res.ok) {
+          return res.json();
+        } else {
+          return res.json().then((data) => {
+            let errorMsg = "Something went wrong. Store was not created!";
+            throw new Error(errorMsg);
+          });
+        }
+      })
+      .then((data) => {
+        setTimeout(async () => {
+          await authCtx.storeUser(userToken);
+          Swal.fire({
+            title: "Store",
+            text: "Store Created Successfully",
+            icon: "success",
+            confirmButtonText: "Close",
+          });
+          setIsLoading(false);
+          history.replace("/dashboard/getting-started");
+        }, 2000);
+      })
+      .catch((err) => {
+        Swal.fire({
+          title: "Store",
+          text: err.message,
+          icon: "error",
+          confirmButtonText: "Close",
+        });
+      });
+  }
+  return (
+    <>
+      <form className="w-full mx-auto text-center" onSubmit={submitHandler}>
+        <FormGroup>
+          <input
+            className="w-full border border-gray-300 rounded-sm px-4 py-3 outline-none transition-colors duration-150 ease-in-out focus:border-blue-400"
+            type="text"
+            required
+            id="name"
+            ref={nameInputRef}
+            placeholder="Name of your store"
+          />
+        </FormGroup>
+        <FormGroup>
+          {!isLoading && <Button text="Create my store" submit full />}
+          {isLoading && (
+            <div
+              className="w-full px-6 py-3 rounded-sm border text-gray-800 bg-gray-200 border-gray-300"
+              role="alert"
+            >
+              Creating store...
+            </div>
+          )}
+        </FormGroup>
+      </form>
+    </>
+  );
+};
 
 export default CreateStoreForm;
--- frontend/src/pages/GettingStarted.js
+++ frontend/src/pages/GettingStarted.js
@@ -1,141 +1,144 @@
-import Card from "../components/ui/Card"
-import { useState, useEffect, useContext } from "react"
+import Card from "../components/ui/Card";
+import { useState, useEffect, useContext } from "react";
 import { Link, useHistory } from "react-router-dom";
 import AuthContext from "../context/auth-context";
 
 function GettingStartedPage() {
+  const history = useHistory();
 
-    const history = useHistory();
+  const authCtx = useContext(AuthContext);
 
-    const authCtx = useContext(AuthContext);
+  const [userStore, setUserHasStore] = useState(false);
+  const [userWallet, setUserHasWallet] = useState(false);
+  const [userLightning, setUserHasLightning] = useState(false);
+  const [userOnboarding, completedOnboarding] = useState(false);
 
-    const [userStore, setUserHasStore] = useState(false);
-    const [userWallet, setUserHasWallet] = useState(false);
-    const [userLightning, setUserHasLightning] = useState(false);
-    const [userOnboarding, completedOnboarding] = useState(false);
+  const userToken = localStorage.getItem("token");
 
-    const userToken = localStorage.getItem('token');
+  const localStore = JSON.parse(localStorage.getItem("storeData"));
 
-    const localStore = JSON.parse(localStorage.getItem('storeData'));
+  const currentUser = JSON.parse(localStorage.getItem("user"));
 
-    const currentUser = JSON.parse(localStorage.getItem('user'));
+  const [isLoading, setIsLoading] = useState(false);
 
-    const [isLoading, setIsLoading] = useState(false);
+  const logoutHandler = () => {
+    authCtx.logout();
+  };
 
-    const logoutHandler = () => {
-        authCtx.logout();
-    }
+  useEffect(() => {
+    setIsLoading(true);
+    const checkWallet = () => {
+      if (localStorage.getItem("walletData")) {
+        setUserHasWallet(true);
+      } else if (currentUser?.data?.store?.wallet?.bitcoin) {
+        setUserHasWallet(true);
+      }
+    };
 
-    useEffect(() => {
-        setIsLoading(true);
-
-        const checkStore = () => {
-            if (localStore) {
-                setUserHasStore(true);
-            } else if (currentUser?.data?.store !== null) {
-                setUserHasStore(true);
-            }
-        };
-
-        const checkWallet = () => {
-            if (localStorage.getItem('walletData')) {
-                setUserHasWallet(true);
-            } else if (currentUser?.data?.store?.wallet?.bitcoin) {
-                setUserHasWallet(true);
-            }
-        };
-
-        const checkLightning = () => {
-            fetch("http://localhost:5000/wallet/light",
-                {
-                    headers: {
-                        'Authorization': `Bearer ${userToken}`
-                    }
-                }).then((res) => {
-                    if (res.ok) {
-                        return res.json();
-
-                    } else {
-                        return res.json().then(data => {
-                            let errorMsg = 'Failed to connect!';
-                            throw new Error(errorMsg);
-                        });
-                    }
-                }).then(data => {
-                    if (data.message === "We are good") {
-                        setUserHasLightning(true);
-                    }
-                    localStorage.setItem('nodeStatus', 'online');
-                })
-                .catch(err => {
-                    alert(err.message);
-                });
-        };
-
-        checkStore();
+    const checkLightning = () => {
+      fetch("http://localhost:5000/wallet/light", {
+        headers: {
+          Authorization: `Bearer ${userToken}`,
+        },
+      })
+        .then((res) => {
+          if (res.ok) {
+            return res.json();
+          } else {
+            return res.json().then((data) => {
+              let errorMsg = "Failed to connect!";
+              throw new Error(errorMsg);
+            });
+          }
+        })
+        .then((data) => {
+          if (data.message === "We are good") {
+            setUserHasLightning(true);
+          }
+          localStorage.setItem("nodeStatus", "online");
+        })
+        .catch((err) => {
+          alert(err.message);
+        });
+    };
+    const checkStore = () => {
+      if (localStore) {
+        setUserHasStore(true);
         checkWallet();
         checkLightning();
+        console.log("Happy");
+      } else if (currentUser?.data?.store !== null) {
+        console.log("Happy");
+        setUserHasStore(true);
+        checkWallet();
+        checkLightning();
+      }
+    };
 
-        setIsLoading(false);
-    });
+    checkStore();
 
-    if (!!userStore && !!userWallet && !!userLightning) {
-        completedOnboarding(true);
+    setIsLoading(false);
+  });
 
-        if (userOnboarding) {
-            history.replace("/dashboard/overview");
+  if (!!userStore && !!userWallet && !!userLightning) {
+    completedOnboarding(true);
 
-        }
+    if (userOnboarding) {
+      history.replace("/dashboard/overview");
     }
+  }
 
-    return (
-        <div className="mb-10">
-            <h2 className="text-xl text-dark font-bold mb-4">Next steps?</h2>
-            {isLoading ? (
-                <p>Checking records...</p>
-            ) : (
-                <>
-                    {!userStore &&
-                        <div className="my-6">
-                            <Card
-                                title="Create your store"
-                                text="Your store is necessary before you can issue invoices"
-                                buttonText="Create store"
-                                buttonLink="/dashboard/create-store"
-                            />
-                        </div>
-                    }
-                    {!userWallet &&
-                        <div className="my-6">
-                            <Card
-                                title="Connect your BTC wallet"
-                                text="Add your xPub key from which recieving addresses will be generated"
-                                buttonText="Add xPub key"
-                                buttonLink="/dashboard/wallets/bitcoin"
-                            />
-                        </div>
-                    }
-                    {!userLightning &&
-                        <div className="my-6">
-                            <Card
-                                title="Connect to a lightning node"
-                                text="Configure BPP to communicate with your lnd node via REST proxy"
-                                buttonText="Connect lnd node"
-                                buttonLink="/dashboard/wallets/lightning"
-                            />
-                        </div>
-                    }
+  return (
+    <div className="mb-10">
+      <h2 className="text-xl text-dark font-bold mb-4">Next steps?</h2>
+      {isLoading ? (
+        <p>Checking records...</p>
+      ) : (
+        <>
+          {!userStore && (
+            <div className="my-6">
+              <Card
+                title="Create your store"
+                text="Your store is necessary before you can issue invoices"
+                buttonText="Create store"
+                buttonLink="/dashboard/create-store"
+              />
+            </div>
+          )}
+          {!userWallet && userStore && (
+            <div className="my-6">
+              <Card
+                title="Connect your BTC wallet"
+                text="Add your xPub key from which recieving addresses will be generated"
+                buttonText="Add xPub key"
+                buttonLink="/dashboard/wallets/bitcoin"
+              />
+            </div>
+          )}
+          {!userLightning && userStore && (
+            <div className="my-6">
+              <Card
+                title="Connect to a lightning node"
+                text="Configure BPP to communicate with your lnd node via REST proxy"
+                buttonText="Connect lnd node"
+                buttonLink="/dashboard/wallets/lightning"
+              />
+            </div>
+          )}
 
-                    <div className="my-6 text-center">
-                        <Link className='no-underline text-md text-red-600 leading-none' to="/" onClick={logoutHandler}>
-                            Logout
-                        </Link>
-                    </div>
-                </>
-            )}
-        </div>
-
-    )
+          <div className="my-6 text-center">
+            <Link
+              className="no-underline text-md text-red-600 leading-none"
+              to="/"
+              onClick={logoutHandler}
+            >
+              Logout
+            </Link>
+          </div>
+        </>
+      )}
+    </div>
+  );
 }
 
 export default GettingStartedPage;
